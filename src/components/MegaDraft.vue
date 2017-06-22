681e971656a8a2a8b82c3d24ed6856b87c10b6fd
Patrick Lee <me@patricklee.nyc>
2017-06-22 12:57:17 -0400

    Pass vue instance into react

--- src/components/MegaDraft.vue
+++ src/components/MegaDraft.vue
@@ -7,17 +7,28 @@
 <script>
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {MegadraftEditor, editorStateFromRaw} from 'megadraft'
+import {MegadraftEditor, editorStateFromRaw, editorStateToJSON} from 'megadraft'
 
 class App extends React.Component {
   constructor (props) {
+    console.log(props)
     super(props)
+    this.setData = props.dataContainer
     this.state = {editorState: editorStateFromRaw(null)}
     this.onChange = this.onChange.bind(this)
+    this.onSaveClick = this.onSaveClick.bind(this)
   }
 
   onChange (editorState) {
     this.setState({editorState})
+  }
+
+  onSaveClick () {
+    const {editorState} = this.state
+    const content = editorStateToJSON(editorState)
+    // Your function to save the content
+    // save_my_content(content)
+    console.log(content)
   }
 
   render () {
@@ -29,13 +40,16 @@ export default {
   name: 'hello',
   mounted () {
     ReactDOM.render(
-      React.createElement(App),
+      React.createElement(App, {vue: this}),
       document.getElementById('megadraft')
     )
   },
   data () {
     return {
+      content: ''
     }
+  },
+  methods: {
   }
 }
 </script>
