0fcde57b3a38fd6206e0142a2129a260b7cf769a
Patrick Lee <plee@penguinrandomhouse.com>
2017-06-22 10:34:33 -0400

    Init

---
+++ src/components/MegaDraft.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id='megadraft'>
+
+  </div>
+</template>
+
+<script>
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {MegadraftEditor, editorStateFromRaw} from 'megadraft'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {editorState: editorStateFromRaw(null)}
+    this.onChange = this.onChange.bind()
+  }
+
+  onChange (editorState) {
+    this.setState({editorState})
+  }
+
+  render () {
+    return React.createElement(MegadraftEditor, {editorState: this.state.editorState, onChange: this.onChange}, null)
+    // return (
+    //   <MegadraftEditor
+    //     editorState={this.state.editorState}
+    //     onChange={this.onChange}/>
+    // )
+  }
+}
+
+export default {
+  name: 'hello',
+  mounted () {
+    ReactDOM.render(
+      React.createElement(App),
+      document.getElementById('megadraft')
+    )
+    // ReactDOM.render(
+    //   <App />,
+    //   document.getElementById('megadraft')
+    // )
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import "../../node_modules/megadraft/dist/css/megadraft.css";
+</style>
